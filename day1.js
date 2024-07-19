6367ae121e13394d02ed7b200125cb7d94938922
Sahil- Bhardwaj29 <sahilsharma291105@gmail.com>
2024-07-19 21:58:25 +0530

    added day1.js file

---
+++ day1.js
@@ -0,0 +1,60 @@
+//Activity 1
+var a =20;
+console.log(a);
+let b ="sahil";
+console.log(b);
+
+//Activity 2
+const c=true;
+console.log(c);
+
+//Activity 3
+var num=10;
+var str="sahil sharma";
+var bool =false;
+var arr=[1,2,3];
+var obj={
+    name:"sahil",
+    rollno:5082
+}
+console.log(typeof num,typeof str,typeof bool,typeof arr,typeof obj);
+
+//Activity 4
+let x=5;
+console.log(x);
+x=10;
+console.log(x);
+
+//Activity 5
+const y=10;
+//y=15    //Error cannot assign to const
+
+//Feature Request
+//task1
+var one =10;
+console.log(one , typeof one);
+var two="hello";
+console.log(two , typeof two);
+var three=[1,2,3,4];
+console.log(three , typeof three);
+var four ={
+    name:"sahil",
+    rollno:5082
+}
+console.log(four , typeof four);
+var five =false;
+console.log(five , typeof five);
+
+//task2
+let check=true;
+console.log(check);
+check=false;
+console.log(check);//can be reassigned
+
+const check2=true;
+console.log(check2);
+//check2 =false;
+// console.log(check2);  // cannot be reassigned
+
+
+
